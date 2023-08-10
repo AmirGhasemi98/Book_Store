91464e85ad277da0bbf1f95f8f7c7188261c65c8
Amir <amirghasemi615@gmail.com>
2023-08-10 17:07:49 +0430

    Add Auto Mapper

---
+++ Book_Store.Application/Profiles/MappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Book_Store.Application.Profiles
+{
+    public class MappingProfile : Profile
+    {
+
+    }
+}
