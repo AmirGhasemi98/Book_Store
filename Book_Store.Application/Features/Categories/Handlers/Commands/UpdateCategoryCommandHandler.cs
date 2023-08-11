4402f06b6687144492c831b676b5799c49e3c122
Amir <amirghasemi615@gmail.com>
2023-08-11 20:03:30 +0430

    Add Category Update Command

---
+++ Book_Store.Application/Features/Categories/Handlers/Commands/UpdateCategoryCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using Book_Store.Application.Features.Categories.Requests.Commands;
+using Book_Store.Application.Persistence.Contracts;
+using MediatR;
+
+namespace Book_Store.Application.Features.Categories.Handlers.Commands
+{
+    public class UpdateCategoryCommandHandler : IRequestHandler<UpdateCategoryCommand, Unit>
+    {
+        private readonly ICategoryRepository _categoryRepository;
+        private readonly IMapper _mapper;
+
+        public UpdateCategoryCommandHandler(ICategoryRepository categoryRepository, IMapper mapper)
+        {
+            _categoryRepository = categoryRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<Unit> Handle(UpdateCategoryCommand request, CancellationToken cancellationToken)
+        {
+            var category = await _categoryRepository.Get(request.CategoryDto.Id);
+            _mapper.Map(request.CategoryDto, category);
+            await _categoryRepository.Update(category);
+            return Unit.Value;
+
+        }
+    }
+}
---
+++ Book_Store.Application/Features/Categories/Requests/Commands/UpdateCategoryCommand.cs
@@ -0,0 +1,10 @@
+﻿using Book_Store.Application.DTOs.Category;
+using MediatR;
+
+namespace Book_Store.Application.Features.Categories.Requests.Commands
+{
+    public class UpdateCategoryCommand : IRequest<Unit>
+    {
+        public CategoryDto CategoryDto { get; set; }
+    }
+}
