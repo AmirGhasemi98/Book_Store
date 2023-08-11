fbea61227e386790c5a52eaed1f91cee682a1f37
Amir <amirghasemi615@gmail.com>
2023-08-11 20:08:21 +0430

    Add Book Update Command

---
+++ Book_Store.Application/Features/Books/Handlers/Commands/UpdateBookCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using Book_Store.Application.Features.Books.Requests.Commands;
+using Book_Store.Application.Persistence.Contracts;
+using MediatR;
+
+namespace Book_Store.Application.Features.Books.Handlers.Commands
+{
+    public class UpdateBookCommandHandler : IRequestHandler<UpdateBookCommand, Unit>
+    {
+        private readonly IBookRepository _bookRepository;
+        private readonly IMapper _mapper;
+
+        public UpdateBookCommandHandler(IBookRepository bookRepository, IMapper mapper)
+        {
+            _bookRepository = bookRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<Unit> Handle(UpdateBookCommand request, CancellationToken cancellationToken)
+        {
+            var book = await _bookRepository.Get(request.BookDto.Id);
+            _mapper.Map(request.BookDto, book);
+            await _bookRepository.Update(book);
+            return Unit.Value;
+        }
+    }
+}
---
+++ Book_Store.Application/Features/Books/Requests/Commands/UpdateBookCommand.cs
@@ -0,0 +1,10 @@
+﻿using Book_Store.Application.DTOs.Book;
+using MediatR;
+
+namespace Book_Store.Application.Features.Books.Requests.Commands
+{
+    public class UpdateBookCommand : IRequest<Unit>
+    {
+        public BookDto BookDto { get; set; }
+    }
+}
