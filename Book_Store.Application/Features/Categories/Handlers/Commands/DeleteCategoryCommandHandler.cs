93b1f7181da23bf3c1eb2b7a37b2650948c4447c
Amir <amirghasemi615@gmail.com>
2023-08-12 20:36:23 +0430

    Add Category Delete Command

---
+++ Book_Store.Application/Features/Categories/Handlers/Commands/DeleteCategoryCommandHandler.cs
@@ -0,0 +1,18 @@
+﻿using Book_Store.Application.Features.Categories.Requests.Commands;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Book_Store.Application.Features.Categories.Handlers.Commands
+{
+    public class DeleteCategoryCommandHandler : IRequestHandler<DeleteCategoryCommand>
+    {
+        public Task<Unit> Handle(DeleteCategoryCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Book_Store.Application/Features/Categories/Requests/Commands/DeleteCategoryCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace Book_Store.Application.Features.Categories.Requests.Commands
+{
+    public class DeleteCategoryCommand : IRequest
+    {
+        public int Id { get; set; }
+    }
+}
