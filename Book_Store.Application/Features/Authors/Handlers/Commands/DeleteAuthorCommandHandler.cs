e21d3d472f47819a8c47f49ae37138acc8174c0b
Amir <amirghasemi615@gmail.com>
2023-08-12 20:35:36 +0430

    Add Author Delete Command

---
+++ Book_Store.Application/Features/Authors/Handlers/Commands/DeleteAuthorCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using Book_Store.Application.Features.Authors.Requests.Commands;
+using Book_Store.Application.Persistence.Contracts;
+using MediatR;
+
+namespace Book_Store.Application.Features.Authors.Handlers.Commands
+{
+    public class DeleteAuthorCommandHandler : IRequestHandler<DeleteAuthorCommand>
+    {
+        private readonly IAuthorRepository _authorRepository;
+        private readonly IMapper _mapper;
+
+        public DeleteAuthorCommandHandler(IAuthorRepository authorRepository, IMapper mapper)
+        {
+            _authorRepository = authorRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<Unit> Handle(DeleteAuthorCommand request, CancellationToken cancellationToken)
+        {
+            var author = await _authorRepository.Get(request.Id);
+            await _authorRepository.Delete(author);
+            return Unit.Value;
+        }
+    }
+}
---
+++ Book_Store.Application/Features/Authors/Requests/Commands/DeleteAuthorCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace Book_Store.Application.Features.Authors.Requests.Commands
+{
+    public class DeleteAuthorCommand : IRequest
+    {
+        public int Id { get; set; }
+    }
+}
