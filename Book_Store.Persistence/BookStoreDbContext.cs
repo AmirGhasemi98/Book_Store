3f92a1f2d1ab974e81b93e1eab6a5141bc717151
Amir <amirghasemi615@gmail.com>
2023-08-17 15:46:07 +0430

    Create BookStoreDbContext

--- Book_Store.Persistence/BookStoreDbContext.cs
+++ Book_Store.Persistence/BookStoreDbContext.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Book_Store.Domain.Common;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,47 @@ using System.Threading.Tasks;
 
 namespace Book_Store.Persistence
 {
-    internal class BookStoreDbContext
+    public class BookStoreDbContext : DbContext
     {
+        public BookStoreDbContext(DbContextOptions<BookStoreDbContext> options) : base(options)
+        {
+
+        }
+
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(BookStoreDbContext).Assembly);
+        }
+
+        public override Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = default)
+        {
+            foreach (var entry in ChangeTracker.Entries<BaseDomainEntity>())
+            {
+                if (entry.State == EntityState.Modified)
+                    entry.Entity.LastModifiedDate = DateTime.UtcNow;
+
+                if (entry.State == EntityState.Added)
+                    entry.Entity.DateCreated = DateTime.UtcNow;
+            }
+
+            return base.SaveChangesAsync(acceptAllChangesOnSuccess, cancellationToken);
+        }
+
+        public override int SaveChanges()
+        {
+            foreach (var entry in ChangeTracker.Entries<BaseDomainEntity>())
+            {
+                if (entry.State == EntityState.Modified)
+                    entry.Entity.LastModifiedDate = DateTime.UtcNow;
+
+                if (entry.State == EntityState.Added)
+                    entry.Entity.DateCreated = DateTime.UtcNow;
+            }
+
+            return base.SaveChanges();
+        }
+
     }
 }
