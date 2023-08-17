0019657c004f98633d77d6968fdd959b5da491c6
Amir <amirghasemi615@gmail.com>
2023-08-17 16:43:57 +0430

    Create PersistenceServicesRegistration.cs

---
+++ Book_Store.Persistence/PersistenceServicesRegistration.cs
@@ -0,0 +1,30 @@
+﻿using Book_Store.Application.Persistence.Contracts;
+using Book_Store.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Book_Store.Persistence
+{
+    public static class PersistenceServicesRegistration
+    {
+        public static IServiceCollection ConfigurePersistenceServices(this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.AddDbContext<BookStoreDbContext>(option =>
+            {
+                option.UseSqlServer(configuration.GetConnectionString("BookStoreConnectionString"));
+            });
+
+            services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+
+            services.AddScoped<IAuthorRepository, AuthorRepository>();
+            services.AddScoped<IPublisherRepository, PublisherRepository>();
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddScoped<IBookRepository , BookRepository>();
+            services.AddScoped<IBookRepository , BookRepository>();
+
+            return services;
+        }
+    }
+}
