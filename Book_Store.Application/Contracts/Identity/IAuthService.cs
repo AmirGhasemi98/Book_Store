455925892798ee010473a202b15b7a69113e3649
Amir <amirghasemi615@gmail.com>
2023-08-18 10:14:57 +0430

    Add Identity Project

---
+++ Book_Store.Application/Contracts/Identity/IAuthService.cs
@@ -0,0 +1,11 @@
+﻿using Book_Store.Application.Models.Identity;
+
+namespace Book_Store.Application.Contracts.Identity
+{
+    public interface IAuthService
+    {
+        Task<AuthResponse> Login(AuthRequset requset);
+
+        Task<RegistrationResponse> Register(RegistrationRequest request);
+    }
+}
---
+++ Book_Store.Identity/BookStoreIdentityDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Book_Store.Identity.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Book_Store.Identity
+{
+    public class BookStoreIdentityDbContext : IdentityDbContext<ApplicationUser>
+    {
+    }
+}
