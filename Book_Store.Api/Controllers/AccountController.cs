4b275d148257bd77ca55aca2235b48a91e4ca2a3
Amir <amirghasemi615@gmail.com>
2023-08-19 20:29:14 +0430

    Set Swagger Settings

---
+++ Book_Store.Api/Controllers/AccountController.cs
@@ -0,0 +1,31 @@
+﻿using Book_Store.Application.Contracts.Identity;
+using Book_Store.Application.Models.Identity;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Book_Store.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+
+        public AccountController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("Login")]
+        public async Task<ActionResult<AuthResponse>> Login(AuthRequset requset)
+        {
+            return Ok(await _authService.Login(requset));
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<RegistrationResponse>> Register(RegistrationRequest request)
+        {
+            return Ok(await _authService.Register(request));
+        }
+    }
+}
--- Book_Store.Api/Program.cs
+++ Book_Store.Api/Program.cs
@@ -1,5 +1,7 @@
 using Book_Store.Application;
+using Book_Store.Identity;
 using Book_Store.Persistence;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,9 +12,11 @@ builder.Services.AddControllers();
 
 builder.Services.ConfigureApplicationServices();
 builder.Services.ConfigurePersistenceServices(builder.Configuration);
+builder.Services.ConfigureIdentityServices(builder.Configuration);
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+//builder.Services.AddSwaggerGen();
+AddSwagger(builder.Services);
 
 builder.Services.AddCors(o =>
 {
@@ -32,6 +36,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseAuthentication();
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
@@ -41,3 +47,47 @@ app.UseCors("CorsPolicy");
 app.MapControllers();
 
 app.Run();
+
+
+void AddSwagger(IServiceCollection services)
+{
+    services.AddSwaggerGen(o =>
+    {
+        o.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+        {
+            Description = @"JWT Authorization Header Using The Bearer Schema.
+                       Enter 'Bearer' [Space] And Then Your Token In The Text Input Below.
+                       Example: 'Bearer 1234ssdw'",
+            Name = "Athorization",
+            In = ParameterLocation.Header,
+            Type = SecuritySchemeType.ApiKey,
+            Scheme = "Bearer"
+        });
+
+        o.AddSecurityRequirement(new OpenApiSecurityRequirement()
+        {
+            {
+               new OpenApiSecurityScheme()
+               {
+                   Reference = new OpenApiReference()
+                   {
+                       Type = ReferenceType.SecurityScheme,
+                       Id = "Bearer"
+                   },
+                   Scheme = "oauth2",
+                   Name = "Bearer",
+                   In = ParameterLocation.Header
+               },
+               new List<string>()
+            }
+
+        });
+
+        o.SwaggerDoc("V1", new OpenApiInfo()
+        {
+            Version = "v1",
+            Title = "Book store Api"
+        });
+    });
+}
+
