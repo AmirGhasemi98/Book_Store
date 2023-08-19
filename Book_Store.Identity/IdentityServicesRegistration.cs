db3dab8adfbcd36b1632f687b15a91dc9b5bb440
Amir <amirghasemi615@gmail.com>
2023-08-19 19:17:17 +0430

    Create Identity Services Registration

---
+++ Book_Store.Identity/IdentityServicesRegistration.cs
@@ -0,0 +1,58 @@
+﻿using Book_Store.Application.Contracts.Identity;
+using Book_Store.Application.Models.Identity;
+using Book_Store.Identity.Models;
+using Book_Store.Identity.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace Book_Store.Identity
+{
+    public static class IdentityServicesRegistration
+    {
+        public static IServiceCollection ConfigureIdentityServices(this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.Configure<JwtSettings>(configuration.GetSection("JwtSettings"));
+
+            services.AddDbContext<BookStoreIdentityDbContext>(option =>
+            {
+                option.UseSqlServer(configuration.GetConnectionString("BookStoreConnectionString"),
+                    b => b.MigrationsAssembly(typeof(BookStoreIdentityDbContext).Assembly.FullName));
+            });
+
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<BookStoreIdentityDbContext>().AddDefaultTokenProviders();
+
+            services.AddTransient<IAuthService, AuthService>();
+            //services.AddTransient<IUserService, UserService>();
+
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+                .AddJwtBearer(o =>
+                {
+                    o.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ClockSkew = TimeSpan.Zero,
+                        ValidIssuer = configuration["Jwtsettings:Issuer"],
+                        ValidAudience = configuration["Jwtsettings:Audience"],
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["Jwtsettings:Audience"]))
+                    };
+                });
+
+            return services;
+        }
+    }
+}
