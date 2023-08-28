61f35a00793400ec7e34fae023dae262bd4edec3
Amir <amirghasemi615@gmail.com>
2023-08-28 19:56:52 +0430

    Create BookImagesController.cs

---
+++ Book_Store.Api/Areas/Admin/Controllers/BookImagesController.cs
@@ -0,0 +1,33 @@
+﻿using Book_Store.Application.Features.BookImages.Requests.Queries;
+using Book_Store.Infrastructure.Extentions;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Book_Store.Api.Areas.Admin.Controllers
+{
+    [Route("api/[Area]/[controller]")]
+    [ApiController]
+    [Area("Admin")]
+    //[Authorize(Roles = "Adminstrator")]
+    public class BookImagesController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public BookImagesController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        // GET api/<BookImagesController>/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var book = await _mediator.Send(new GetBookImageRequest { BookId = id });
+
+            var contentType = book.Image.ContentType();
+
+            return File(book.Image, contentType, book.Image.FileName());
+        }
+
+    }
+}
