86790b4d1189b948479546f11a2d0e67cb523c74
Amir <amirghasemi615@gmail.com>
2023-08-28 19:57:56 +0430

    Add Token Repository

---
+++ Book_Store.Application/Contracts/Persistence/ITokenRepository.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Book_Store.Application.Contracts.Persistence
+{
+    public interface ITokenRepository : IGenericRepository<IdentityUserToken<int>>
+    {
+    }
+}
---
+++ Book_Store.Persistence/Repositories/TokenRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Book_Store.Persistence.Repositories
+{
+    internal class TokenRepository
+    {
+    }
+}
