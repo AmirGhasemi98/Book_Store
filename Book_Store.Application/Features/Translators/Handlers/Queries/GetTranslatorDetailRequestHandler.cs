30a89706905331c205ff7885f28ff1a8f5027977
Amir <amirghasemi615@gmail.com>
2023-08-31 19:44:38 +0430

    Add Translator detail Request

---
+++ Book_Store.Application/Features/Translators/Handlers/Queries/GetTranslatorDetailRequestHandler.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using Book_Store.Application.Contracts.Persistence;
+using Book_Store.Application.DTOs.Translator;
+using Book_Store.Application.Features.Translators.Requests.Queries;
+using MediatR;
+
+namespace Book_Store.Application.Features.Translators.Handlers.Queries
+{
+    public class GetTranslatorDetailRequestHandler : IRequestHandler<GetTranslatorDetailRequest, TranslatorDto>
+    {
+        private readonly ITranslatorRepository _translatorRepository;
+        private readonly IMapper _mapper;
+
+        public GetTranslatorDetailRequestHandler(ITranslatorRepository translatorRepository, IMapper mapper)
+        {
+            _translatorRepository = translatorRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<TranslatorDto> Handle(GetTranslatorDetailRequest request, CancellationToken cancellationToken)
+        {
+            var translator = await _translatorRepository.Get(request.Id);
+            return _mapper.Map<TranslatorDto>(translator);
+        }
+    }
+}
---
+++ Book_Store.Application/Features/Translators/Requests/Queries/GetTranslatorDetailRequest.cs
@@ -0,0 +1,10 @@
+﻿using Book_Store.Application.DTOs.Translator;
+using MediatR;
+
+namespace Book_Store.Application.Features.Translators.Requests.Queries
+{
+    public class GetTranslatorDetailRequest : IRequest<TranslatorDto>
+    {
+        public int Id { get; set; }
+    }
+}
