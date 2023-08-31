684c69ce8bcbafb8e94d2749fed6a9163dc682cb
Amir <amirghasemi615@gmail.com>
2023-08-31 17:41:06 +0430

    Migration Update Book Image

---
+++ Book_Store.Persistence/Migrations/20230831130142_UpdateBookImageEntity.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Book_Store.Persistence.Migrations
+{
+    public partial class UpdateBookImageEntity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ContentType",
+                table: "BookImages",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FileName",
+                table: "BookImages",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ContentType",
+                table: "BookImages");
+
+            migrationBuilder.DropColumn(
+                name: "FileName",
+                table: "BookImages");
+        }
+    }
+}
