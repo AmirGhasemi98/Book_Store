74baa0a405406b7cf39add0d1952aa92c6d7e71b
Amir <amirghasemi615@gmail.com>
2023-09-14 19:01:19 +0430

    Add User Manager Repository

---
+++ Book_Store.Application/Contracts/Identity/IUserManagerRepository.cs
@@ -0,0 +1,22 @@
+﻿using Book_Store.Domain.Identity;
+using Microsoft.AspNetCore.Identity;
+
+namespace Book_Store.Application.Contracts.Identity
+{
+    public interface IUserManagerRepository
+    {
+        Task<List<ApplicationUser>> GetList();
+
+        Task<ApplicationUser> Get(int id);
+
+        Task<List<IdentityRole<int>>> GetUserRoles(ApplicationUser user);
+
+        Task<List<string>> AddRoleToUser(ApplicationUser user, List<IdentityRole<int>> roles);
+
+        Task<List<string>> Add(ApplicationUser user, List<IdentityRole<int>> roles, string password);
+
+        Task<List<string>> Edit(ApplicationUser user, List<IdentityRole<int>> roles, string password);
+
+        Task<List<string>> Delete(ApplicationUser user);
+    }
+}
---
+++ Book_Store.Persistence/Repositories/UserManagerRepository.cs
@@ -0,0 +1,150 @@
+﻿using Book_Store.Application.Contracts.Identity;
+using Book_Store.Domain.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Book_Store.Persistence.Repositories
+{
+    public class UserManagerRepository : IUserManagerRepository
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole<int>> _roleManager;
+
+        public UserManagerRepository(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole<int>> roleManager)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        public async Task<List<ApplicationUser>> GetList()
+        {
+            return _userManager.Users.ToList();
+        }
+
+        public async Task<ApplicationUser> Get(int id)
+        {
+            var user = await _userManager.FindByIdAsync(id.ToString());
+            return user;
+        }
+
+        public async Task<List<IdentityRole<int>>> GetUserRoles(ApplicationUser user)
+        {
+            var roles = _roleManager.Roles.ToList();
+
+            List<IdentityRole<int>> userRoles = new List<IdentityRole<int>>();
+
+            foreach (var role in roles)
+            {
+                if (await _userManager.IsInRoleAsync(user, role.Name))
+                {
+                    userRoles.Add(role);
+                }
+            }
+
+            return userRoles;
+        }
+
+        public async Task<List<string>> Add(ApplicationUser user, List<IdentityRole<int>> roles, string password)
+        {
+            List<string> messages = new List<string>();
+
+            var result = await _userManager.CreateAsync(user, password);
+
+            if (result.Succeeded)
+            {
+                if (roles is not null && roles.Any())
+                {
+                    var roleResult = await AddRoleToUser(user, roles);
+
+                    if (roleResult.Any())
+                        return roleResult;
+                }
+
+                return messages;
+            }
+
+            messages.AddRange(result.Errors.Select(e => e.Description));
+
+            return messages;
+        }
+
+        public async Task<List<string>> AddRoleToUser(ApplicationUser user, List<IdentityRole<int>> roles)
+        {
+            List<string> messages = new List<string>();
+
+            var allRoles = await _roleManager.Roles.ToListAsync();
+
+            var removeResult = await _userManager.RemoveFromRolesAsync(user, allRoles.Select(x => x.Name));
+
+            if (!removeResult.Succeeded)
+            {
+                messages.AddRange(removeResult.Errors.Select(e => e.Description));
+
+                return messages;
+            }
+
+            if (roles is not null && roles.Any())
+            {
+                var result = await _userManager.AddToRolesAsync(user, roles.Select(x => x.Name));
+
+                if (result.Succeeded) return messages;
+
+                messages.AddRange(result.Errors.Select(e => e.Description));
+            }
+
+            return messages;
+        }
+
+        public async Task<List<string>> Edit(ApplicationUser user, List<IdentityRole<int>> roles, string password)
+        {
+            List<string> messages = new List<string>();
+
+            var result = await _userManager.UpdateAsync(user);
+
+            if (result.Succeeded)
+            {
+                if (roles is not null && roles.Any())
+                {
+                    var roleResult = await AddRoleToUser(user, roles);
+
+                    if (roleResult.Any())
+                        return roleResult;
+                }
+
+                if (!string.IsNullOrWhiteSpace(password))
+                {
+                    var token = await _userManager.GeneratePasswordResetTokenAsync(user);
+
+                    var passwordResult = await _userManager.ResetPasswordAsync(user, token, password);
+
+                    if (passwordResult.Succeeded)
+                    {
+                        return messages;
+                    }
+
+                    messages.AddRange(passwordResult.Errors.Select(e => e.Description));
+
+                    return messages;
+                }
+
+                return messages;
+            }
+
+            messages.AddRange(result.Errors.Select(e => e.Description));
+
+            return messages;
+        }
+
+        public async Task<List<string>> Delete(ApplicationUser user)
+        {
+            List<string> messages = new List<string>();
+
+            var result = await _userManager.DeleteAsync(user);
+
+            if (!result.Succeeded)
+                messages.AddRange(result.Errors.Select(e => e.Description));
+
+            return messages;
+        }
+    }
+}
