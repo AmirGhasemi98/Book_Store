709c5e744fb1b4d10439dedbefdd08f42ad76905
Amir <amirghasemi615@gmail.com>
2023-09-14 11:25:22 +0430

    Create RolesController.cs

---
+++ Book_Store.Api/Areas/Admin/Controllers/RolesController.cs
@@ -0,0 +1,53 @@
+﻿using Book_Store.Application.Features.Roles.Requests.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Book_Store.Api.Areas.Admin.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RolesController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public RolesController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        // GET: api/<RolesController>
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var roles = await _mediator.Send(new GetRolesListRequest());
+            return Ok(roles);
+        }
+
+        // GET api/<RolesController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<RolesController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<RolesController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<RolesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
